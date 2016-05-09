d80d475e106ed3608d3e8623a8c1e2e796b53885
Lane Lutgen <lutgenlane@gmail.com>
2016-05-08 18:23:22 -0600

    Menu Development

---
+++ Block Breaker 2D/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string levelName)
+    {
+        Debug.Log(levelName + " is attempting to load.");
+        Application.LoadLevel(levelName);
+    }
+}
