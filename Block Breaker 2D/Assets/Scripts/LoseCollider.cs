c2400055414c14f11e3929a64db0f9bef2b50bb8
Lane Lutgen <lutgenlane@gmail.com>
2016-05-30 17:58:10 -0600

    Added paddle and level colliders

---
+++ Block Breaker 2D/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+    void OnTriggerEnter2D (Collider2D trigger)
+    {
+        print("Trigger");
+    }
+
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        print("Collision");
+        levelManager.LoadLevel("Win Screen");
+    }
+
+
+}
