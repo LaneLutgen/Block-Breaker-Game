00ef594055fe0bf3ad83c2301a71bddfd270445f
Lane Lutgen <lutgenlane@gmail.com>
2016-12-11 20:57:17 -0700

    Game done for the most part

---
+++ Block Breaker 2D/Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public PaddleMover paddle;
+
+    private bool hasStarted = false;
+    private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted)
+        {
+            //Lock the ball on the paddle
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+
+            //Wait for mouse click to launch the ball
+            if (Input.GetMouseButton(0))
+            {
+                //Launch that sumbitch
+                hasStarted = true;
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(200f, 300f);
+            }
+        }
+	}
+}
---
+++ Block Breaker 2D/Assets/Scripts/PaddleMover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, this.transform.position.z); 
+
+        float mousePosition = (Input.mousePosition.x / Screen.width * 650);
+
+        print(mousePosition);
+
+        paddlePos.x = Mathf.Clamp(mousePosition, 50f, 650f);
+
+        this.transform.position = paddlePos;
+	}
+}
